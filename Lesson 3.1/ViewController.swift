f24f173e5e015620e8a2a92e23a127a0b45b24d2
Timur Begishev <rpsi-mono@yandex.ru>
2020-11-22 11:52:47 +0300

    finish

--- Lesson 3.1/ViewController.swift
+++ Lesson 3.1/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
+@IBDesignable
+class PrimaryButton: UIButton {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+            
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+                layer.borderColor = newValue?.cgColor
+        }
+    }
+}
+
 class ViewController: UIViewController {
 
+//    @IBOutlet weak var movingView: UIView!
+    @IBOutlet weak var topRectConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func goRectDown(_ sender: PrimaryButton) {
+        topRectConstraint?.constant = (topRectConstraint?.constant ?? 0) + 150
+        sender.isEnabled = false
+        sender.backgroundColor = .gray
+    }
 
 }
 
